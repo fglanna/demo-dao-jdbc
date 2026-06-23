21c66df0b0062ba5498ddbc67f888a5c38686c85
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-06-23 10:18:04 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+
+		Seller seller = new Seller(21, "Flávio", "fgl@xmail.com", 3000.0, new Date(), obj);
+		System.out.println(seller);
 
 	}
 

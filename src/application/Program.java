0e366cd07f53f4ce7c5f6d0ca0350be89d56765a
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-06-23 13:17:15 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
-		Seller seller = new Seller(21, "Flávio", "fgl@xmail.com", 3000.0, new Date(), obj);
+		Seller seller = sellerDao.findById(3);
+
 		System.out.println(seller);
 
 	}

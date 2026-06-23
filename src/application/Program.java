73236775fe5cc51fa798babaec4ccc786d088029
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2026-06-23 11:33:47 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Program {
 
 		Department obj = new Department(1, "Books");
 		System.out.println(obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		Seller seller = new Seller(21, "Flávio", "fgl@xmail.com", 3000.0, new Date(), obj);
 		System.out.println(seller);
